d35721379430dfd4d8a30e95e4a2aed0523cc67d
daniel <danielcastanedadev@gmail.com>
2026-03-29 08:38:41 -0500

    add var

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   youtubeChannelUrl: 'https://www.youtube.com/@IglesiaCristianaC.R.I',
   churchName: 'Iglesia Cristiana C.R.I',
-  youtubeApiKey: 'TU_API_KEY_AQUI',
+  youtubeApiKey: 'AIzaSyCFsPCfgLNccMp-_zit8OU-NFqdTaW467M',
 };
